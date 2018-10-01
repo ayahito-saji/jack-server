60858d2fdcb7596c455be858ab90971efa5543af
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-10-01 16:24:26 +0900

    deviseのインストール

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+slack = Slack::Slack.new
+
+members = slack.getActiveMembersList
+members.each do |member|
+  puts "#{member['id']} #{member['profile']['real_name']}(#{member['profile']['display_name']}) #{member['profile']['email']}"
+end
