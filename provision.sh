7763ed5178e37ce6034c56e136a9e7a73d6ecc60
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-11-08 21:30:05 +0900

    vagrantfileを追加

---
+++ provision.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+apt-get -y install git curl g++ make
+apt-get -y install zlib1g-dev libssl-dev libreadline-dev
+apt-get -y install libyaml-dev libxml2-dev libxslt-dev
+apt-get -y install sqlite3 libsqlite3-dev nodejs
+
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+source ~/.bashrc
+
+rbenv install 2.4.0
+rbenv rehash
+rbenv global 2.4.0
+
+gem update --system
+gem install bundler
+gem install rails --version='5.1.4'
+gem install sqlite3
+gem install therubyracer
+
+cd /vagrant
+
+bundle install --without production
